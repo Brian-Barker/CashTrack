c8e8b04d2a3206249d31efc53e7c729e861fff5b
Max <max@dirocco.com>
2021-08-26 19:23:49 -0400

    Added slide animation to header

--- App.js
+++ App.js
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import {Dimensions} from 'react-native';
-import Animated from 'react-native-reanimated';
+import Animated, {
+  useAnimatedStyle,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
 
 // const [width, height] = Dimensions.get();
 
 const App = () => {
+  const translateY = useSharedValue(-500);
+  const slide = useAnimatedStyle(() => {
+    return {
+      transform: [{translateY: translateY.value}],
+    };
+  });
+
+  useEffect(() => {
+    translateY.value = withTiming(0, {duration: 1000});
+  });
+
   return (
     <Animated.View
       style={{
@@ -14,15 +29,18 @@ const App = () => {
         backgroundColor: '#f8f8ff',
       }}>
       <Animated.View
-        style={{
-          flex: 1,
-          width: '100%',
-          backgroundColor: '#4f08aa',
-          borderBottomLeftRadius: 20,
-          borderBottomRightRadius: 20,
-          alignItems: 'center',
-          justifyContent: 'center',
-        }}>
+        style={[
+          slide,
+          {
+            flex: 1,
+            width: '100%',
+            backgroundColor: '#4f08aa',
+            borderBottomLeftRadius: 20,
+            borderBottomRightRadius: 20,
+            alignItems: 'center',
+            justifyContent: 'center',
+          },
+        ]}>
         <Animated.Text style={{color: 'white', fontSize: 24}}>
           Test
         </Animated.Text>
